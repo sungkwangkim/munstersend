11aeb2ca0baeca63568e58e6469320c73c8f6fe1
sungkwang kim <sungkwang@wantedlab.com>
2023-07-10 21:57:06 +0900

    fix: favicon, footer 수정

--- components/footer.js
+++ components/footer.js
@@ -94,20 +94,7 @@ export default function Footer() {
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a
-            href="https://web3templates.com/"
-            target="_blank"
-            rel="noopener">
-            Web3Templates.
-          </a>{" "}
-          Illustrations from{" "}
-          <a
-            href="https://www.glazestock.com/"
-            target="_blank"
-            rel="noopener ">
-            Glazestock
-          </a>
+          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "} 먼스터샌드.
         </div>
       </Container>
       {/* Do not remove this */}
